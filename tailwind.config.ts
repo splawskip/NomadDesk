9d54d24916cfbcc3ad0350e86d0e7e787dab20e3
psplawski <psplawski.dev@gmail.com>
2024-01-17 19:08:49 +0100

    Declare new colors inside Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+		colors: {
+			primary: 'hsl(var(--color-primary))',
+			secondary: 'hsl(var(--color-secondary))',
+			accent: 'hsl(var(--color-accent))',
+		}
     },
   },
   plugins: [],

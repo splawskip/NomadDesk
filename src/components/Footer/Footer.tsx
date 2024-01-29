017b6419c53b9de0a115c75cbffbaf993338ea51
Shane Spławski <psplawski@placester.com>
2024-01-29 15:23:31 +0100

    Add favicons and logo

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,13 +2,15 @@ import Link from "next/link";
 import VisuallyHidden from "../VisuallyHidden/VisuallyHidden";
 import { SOCIALS } from "@/constants";
 import React from "react";
+import Image from "next/image";
 
 const Footer = () => {
 	const id = React.useId();
 	// Build component.
 	return (
 		<footer className="mt-auto w-full flex flex-col justify-center items-center md:flex-row md:justify-between gap-4 border-b border-gray-100 bg-white-50 text-gray-950 dark:border-gray-600 dark:bg-gray-800 dark:text-white-50 p-6">
-			<Link prefetch={true} href='/'>
+			<Link prefetch={true} href='/' className="flex flex-col md:flex-row justify-start items-center flex-nowrap gap-3">
+				<Image src="/logo.webp" width={45} height={45} loading="lazy" alt="Logo of the site" />
 				<h2 className="text-secondary text-xl font-bold">NomadDesk</h2>
 			</Link>
 			<a href="https://www.themuse.com/developers/api/v2" rel="noopener noreferrer" target="_blank">
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Nav from "../Nav/Nav";
 import ThemeToggle from "../ThemeToggle";
+import Image from "next/image";
 
 type HeaderProps = {
 	theme: string,
@@ -11,7 +12,10 @@ const Header = ({theme} : HeaderProps) => {
 	return (
 		<header className="w-full flex justify-between items-center gap-4 border-b border-gray-100 bg-white-50 text-gray-950 dark:border-gray-600 dark:bg-gray-800 dark:text-white-50 p-6">
 			<div className="flex justify-start items-center gap-4">
-				<Link prefetch={true} href='/'><h1 className="text-secondary text-xl font-bold">NomadDesk</h1></Link>
+				<Link prefetch={true} href='/' className="flex justify-start items-center flex-nowrap gap-3">
+					<Image src="/logo.webp" width={45} height={45} priority={true} alt="Logo of the site"></Image>
+					<h1 className="text-secondary text-xl font-bold">NomadDesk</h1>
+				</Link>
 				<p className="hidden md:block text-secondary text-sm">#1 Job Board for digital nomads</p>
 				<ThemeToggle initialTheme={theme}></ThemeToggle>
 			</div>

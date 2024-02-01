87331cd6382ad649bb09c0a81d678e65f6f491b8
Shane Spławski <psplawski@placester.com>
2024-02-01 16:04:09 +0100

    Increase contrast of the pagination items

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -36,10 +36,10 @@ const Pagination = ({ count = 0, searchParams = {} } : PaginationProps) => {
 
     return (
         <nav className="w-full flex justify-center items-center flex-wrap bg-white-100 dark:bg-gray-900 px-6 pb-6">
-            <ul className="flex gap-px text-base min-h-10 flex-wrap justify-center items-center">
+            <ul className="flex text-base min-h-10 flex-wrap justify-center items-center">
 				{currentPage !== 1 && (
 					<li className="hidden md:block">
-                    	<Link href={`/?${previousUrl.toString()}`} prefetch={true} className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white outline-none focus-visible:relative focus-visible:z-10 focus-visible:ring ring-blue-500"><VisuallyHidden>Previous Page</VisuallyHidden><ArrowLeft /></Link>
+                    	<Link href={`/?${previousUrl.toString()}`} prefetch={true} className="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-950 bg-white-50 border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white outline-none focus-visible:relative focus-visible:z-10 focus-visible:ring ring-blue-500"><VisuallyHidden>Previous Page</VisuallyHidden><ArrowLeft /></Link>
                 	</li>
 				)}
                 {range(startPage, endPage).map((pageNumber) => {
@@ -48,16 +48,15 @@ const Pagination = ({ count = 0, searchParams = {} } : PaginationProps) => {
                     const isCurrent = pageNumber === currentPage;
                     return (
                         <li key={`${id}-${pageNumber}`}>
-                            <Link href={`/?${currentUrl.toString()}`} prefetch={true} className={clsx('flex items-center justify-center px-4 h-10 leading-tight border border-gray-300', isCurrent ? 'bg-white-200 text-gray-950 dark:bg-gray-950 dark:text-white-100 font-semibold' : 'bg-white text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800' ,'dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white outline-none focus-visible:relative focus-visible:z-10 focus-visible:ring ring-blue-500')}>
-								<VisuallyHidden>Page #</VisuallyHidden>
-                                {pageNumber}
+                            <Link href={`/?${currentUrl.toString()}`} prefetch={true} className={clsx('flex items-center justify-center px-4 h-10 leading-tight border border-gray-300 text-gray-950', isCurrent ? 'bg-white-200 dark:bg-gray-950 dark:text-white-100 font-semibold' : 'bg-white-50 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800' ,'dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white outline-none focus-visible:relative focus-visible:z-10 focus-visible:ring ring-blue-500')}>
+								<VisuallyHidden>Page #</VisuallyHidden>{pageNumber}
                             </Link>
                         </li>
                     );
                 })}
 				{currentPage !== (count - 1) && (
                 	<li className="hidden md:block">
-						<Link href={`/?${nextUrl.toString()}`} prefetch={true} className="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white outline-none focus-visible:relative focus-visible:z-10 focus-visible:ring ring-blue-500"><VisuallyHidden>Next Page</VisuallyHidden><ArrowRight /></Link>
+						<Link href={`/?${nextUrl.toString()}`} prefetch={true} className="flex items-center justify-center px-4 h-10 leading-tight text-gray-950 bg-white-50 border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white outline-none focus-visible:relative focus-visible:z-10 focus-visible:ring ring-blue-500"><VisuallyHidden>Next Page</VisuallyHidden><ArrowRight /></Link>
 					</li>
 				)}
             </ul>

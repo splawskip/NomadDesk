1f7f8a507f317abfbe0241ebda89e8299307d254
Shane Spławski <psplawski@placester.com>
2024-02-08 00:43:52 +0100

    Close side menu on Route Change

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -3,19 +3,26 @@
 import { NAV } from "@/constants";
 import clsx from "clsx";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { useId, useState } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useEffect, useId, useState } from "react";
 import { Menu, X } from "react-feather";
 import VisuallyHidden from "../VisuallyHidden";
 
 const Nav = () => {
 	const pathname = usePathname();
+	const searchParams = useSearchParams();
 	const id = useId();
 	const [isSideMenuOpen, setIsSideMenuOpen] = useState<boolean>(false);
+	const toggleSideMenu = () => setIsSideMenuOpen(!isSideMenuOpen);
+
+	useEffect(() => {
+		setIsSideMenuOpen(false);
+	}, [pathname, searchParams]);
+
 	// Build component.
 	return (
 		<>
-			<button type="button" className="block md:hidden" onClick={() => setIsSideMenuOpen(!isSideMenuOpen)}>
+			<button type="button" className="block md:hidden" onClick={toggleSideMenu} >
 				<VisuallyHidden>Open side menu</VisuallyHidden>
 				<Menu />
 			</button>
@@ -30,7 +37,7 @@ const Nav = () => {
 			</nav>
 			{isSideMenuOpen && (
 				<div className="z-30 w-full sm:w-1/2 fixed inset-y-0 right-0 flex flex-col items-end justify-start gap-4 bg-white-100 dark:bg-gray-900 p-6 md:hidden">
-					<button type="button" className="block outline-none focus-visible:ring ring-blue-500 md:hidden" onClick={() => setIsSideMenuOpen(!isSideMenuOpen)}>
+					<button type="button" className="block outline-none focus-visible:ring ring-blue-500 md:hidden" onClick={toggleSideMenu}>
 						<VisuallyHidden>Close side menu</VisuallyHidden>
 						<X />
 					</button>

6e50acdb37bdf8b9c31aeaa32c4eec4f07e9a50c
Shane Spławski <psplawski@placester.com>
2024-02-08 21:58:39 +0100

    Use framer motion to animate theme toggle component

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -7,6 +7,7 @@ import { usePathname, useSearchParams } from "next/navigation";
 import { useEffect, useId, useState } from "react";
 import { Menu, X } from "react-feather";
 import VisuallyHidden from "../VisuallyHidden";
+import { motion } from "framer-motion";
 
 const Nav = () => {
 	const pathname = usePathname();
@@ -22,7 +23,7 @@ const Nav = () => {
 	// Build component.
 	return (
 		<>
-			<button type="button" className="block md:hidden" onClick={toggleSideMenu} >
+			<button type="button" className="block md:hidden hover:text-nomadic-sand-300" onClick={toggleSideMenu} >
 				<VisuallyHidden>Open side menu</VisuallyHidden>
 				<Menu />
 			</button>
@@ -35,9 +36,17 @@ const Nav = () => {
 					))}
 				</ul>
 			</nav>
-			{isSideMenuOpen && (
-				<div className="z-30 w-full sm:w-1/2 fixed inset-y-0 right-0 flex flex-col items-end justify-start gap-4 bg-white-100 dark:bg-gray-900 p-6 md:hidden">
-					<button type="button" className="block outline-none focus-visible:ring ring-blue-500 md:hidden" onClick={toggleSideMenu}>
+			<motion.div
+					layout={true}
+					transition={{
+						type: 'spring',
+						stiffness: 500,
+						damping: 40,
+					}}
+					initial={{x: '100%'}}
+					animate={{x: isSideMenuOpen ? 0 : '100%'}}
+					className="z-30 w-full sm:w-1/2 fixed inset-y-0 right-0 flex flex-col items-end justify-start gap-4 bg-white-100 dark:bg-gray-900 p-6 md:hidden">
+					<button type="button" className="block outline-none focus-visible:ring ring-blue-500 md:hidden hover:text-nomadic-sand-300" onClick={toggleSideMenu}>
 						<VisuallyHidden>Close side menu</VisuallyHidden>
 						<X />
 					</button>
@@ -48,8 +57,7 @@ const Nav = () => {
 							</li>
 						))}
 					</ul>
-				</div>
-			)}
+			</motion.div>
 		</>
 	);
 };
